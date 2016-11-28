4cb875b8b22cc9ae7157a19689ddfe3b89ee20b0
ks404 <ksajaev@gmail.com>
2016-11-28 16:35:55 -0600

    back kwargs

--- metalapp/scraper/spiders.py
+++ metalapp/scraper/spiders.py
@@ -6,13 +6,13 @@ class ListingSpider(DjangoSpider):
     
     name = 'listing_spider'
 
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         self._set_ref_object(SearchTerm, **kwargs)
         self.scraper = self.ref_object.scraper
         self.scrape_url = self.ref_object.url
         self.scheduler_runtime = self.ref_object.scraper_runtime
         self.scraped_obj_class = Listing
         self.scraped_obj_item_class = ListingItem
-        super(ListingSpider, self).__init__(self, *args)
+        super(ListingSpider, self).__init__(self, *args, **kwargs)
 
 #erase **kwargs in line 9 and 16
